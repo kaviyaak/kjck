f5c1914331664ee7391cbf45922fb7ef415b621e
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-21 11:45:44 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int counter=1;
+int a,d,l;
+printf("enter the total no ");
+scanf("%d",&l);
+printf("enter the common diff");
+scanf("%d",&d);
+printf("arithmetic sequence is",l)
+while(counter<=1)
+{
+a=1+(couter-1)*d;
+printf("%d",a);
+counter++;
+}
+}
