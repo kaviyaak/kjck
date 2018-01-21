ef050c4892580766708bc64c0f5fc151abcd3912
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-21 11:19:44 +0530

    Create pimein.c

---
+++ pimein.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+intn,i,count=0;
+int x,y;
+printf("enter the no");
+scanf("%d",&n);
+printf("enter the interval");
+scanf(%d%d",&x,&y);
+if(i>=x&&i<=y)
+{
+for(i=2;i<n;i++)
+{
+ifelse((n%i)==0)
+{
+count++;
+}}
+if(count==0)
+{
+printf("prime");
+}
+else
+{
+pintf("not a prime")
+}
+}
