eb56aeff8022d23efc7ecfe3b9fd92f75bb33975
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-21 10:40:42 +0530

    Create evenin.c

---
+++ evenin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,e,i;
+printf("enter the even intervals");
+scanf("%d",&a);
+scanf("%d",&e);
+for(i=a;i<e;i=++)
+{
+if(i%2===0)
+{
+printf("result is %d",i)
+}
+}
