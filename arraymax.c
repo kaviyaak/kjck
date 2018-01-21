d764e0739e19ae1eb00bd80b5b6024e942c70e80
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-21 12:12:08 +0530

    Create arraymax.c

---
+++ arraymax.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+3include<conio.h>
+void main()
+{
+inta[15];
+int s,i,m;
+printf("enter the size");
+scanf("%d",&s);
+printf("enter the array");
+for(i=0;i<s;i++)
+{
+scanf("%d",&a[i]);
+}
+max=max[0];
+for(i=0;i<s;i++)
+{
+if(max<a[i])
+max=a[i];
+}
+printf("enter the max element");
+}
