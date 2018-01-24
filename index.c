42841e71fdc33839c691c2e70bd1c2b2df2d77a1
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-24 16:54:36 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int[25],i,t;
+printf("enter the size of array");
+scanf("%d",&a[i]);
+printf("enter the element");
+scanf("%d",&t);
+for(i=0;i<t;i++)
+{
+printf(%d%d",a[i],i);
+}
+}
