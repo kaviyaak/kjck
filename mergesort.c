35df5163641609aa833d6771a0f4f64e8fc99570
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-24 16:33:25 +0530

    Create mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[50],b,i,s=4,k=2,temp,j;
+for(i=0;i<d;i++)
+{
+scanf("%d",&a[i]);
+printf("%d",b);
+}
+for(j=0;j<s;j++)
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+printf("%d",a[i]);
+}
