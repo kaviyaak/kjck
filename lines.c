9c0a334619df1c8112b05be9175dd2c1b8f6a1af
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-31 15:55:08 +0530

    Create lines.c

---
+++ lines.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10]="hai friends.good morning";
+int i,c=0;
+for(i=0;a[i]='/0',i++)
+{
+if(a[i]=='')
+count++;
+}
+printf("the no of lines is %d",c);
+}
