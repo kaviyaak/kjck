d4742ec36612013463fd42ab5e864e1c85eebb71
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-31 16:49:18 +0530

    Create spechar.c

---
+++ spechar.c
@@ -0,0 +1,12 @@
+#include<stio.h>
+#include<conio.h>
+void main()
+{
+char a[10]="hai hello.@#$";
+int i,count=0;
+for(i=0;a[i]!='/0';i++)
+{
+if(a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9'||a[i]=='@'||a[i]=='#'||a[i]=='$)
+count++;
+printf("special character is %d",count);
+}
