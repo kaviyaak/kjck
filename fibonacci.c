1d00e4ef4c480b214df1244295efd045e2df15aa
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-31 16:12:48 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int f=0,s=1,N,limit,i;
+printf("enter the limit");
+scanf("%d",&limit);
+printf("%d %d",f,s);
+for(i=2;i<limit;i++)
+{
+N=f+s;
+f=s;
+s=N;
+printf("%d",N);
+}}
