2f73b49de805beeed27247ba1713c04c529a26db
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-01-31 15:47:11 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10]="hai friends";
+int j,c=0;
+for(j=0;a[j]='/0';j++)
+{
+if(a[j]=='')
+count++;
+}
+printf("the words is %d",count+1);
+}
