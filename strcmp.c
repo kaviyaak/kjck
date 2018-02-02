a2fa24222591f6a7d1cf8c78645b39d038524512
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-02 16:43:09 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,18 @@
+#incude<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10],b[10];
+printf("enter the string");
+scanf("%s",&a);
+printf("enter the string");
+scanf("%d",&b);
+if(strcmp(a,b)==0)
+{
+printf("string is equal");
+}
+else
+{
+printf("string not equal");
+}}
