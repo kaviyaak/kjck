d1ba5b9b57aeead2e1c359d5b78921f2e0e680a9
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-02 16:26:12 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int a,i;
+    char b[50];
+    scanf("%d",&a);
+    scanf("%s",&b);
+    for(i=0;i<a;i++)
+    {
+        printf("%s\n",b);
+}
+}
