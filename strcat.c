c6383f00a9aedf640a6387e420220dcf91545659
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-05 16:29:01 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+void main(void) 
+{
+	char a[10],b[20];
+	printf(" enter the 2 strings");
+	scanf("%s %s",&a,&b);
+	printf("concadenated string is %s%s",a,b);
+}
