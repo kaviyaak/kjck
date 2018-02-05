2896798d3a368608a8e67eab278a1a2816664516
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-05 16:01:31 +0530

    Create power2.c

---
+++ power2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i;
+printf("enter the number");
+scanf("%d",&a);
+for(i=1;i<a/2;i++)
+{
+if(a==(i*i))
+printf("yes power of 2");
+}
+else
+printf("not a power of 2");
+}}
