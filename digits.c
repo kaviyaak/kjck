8b0bed26b2bf479f1a3ac032263f7736f398de72
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-07 16:16:53 +0530

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,k,x;
+	printf(" enter the number");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		b=a%10;
+		k=(k*10)+;
+		a=a/10;
+	}
+	while(k!=0)
+	{
+		x=k%10;
+		printf("%d ",x);
+		k=k/10;
+	}
+	return 0;
+}
