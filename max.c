2a689c1f42f0b3be953eb794ef40eea77a85e2c7
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-07 16:27:22 +0530

    Update max.c

--- max.c
+++ max.c
@@ -2,19 +2,19 @@
 #include<conio.h>
 void main()
 {
-int a[i];
+int b[i];
 int i,k,j;
 for(i=0;i<10;i++)
 {
-scanf("%d",&a[i])
+scanf("%d",&b[i])
 }
 for(j=i+1;j<10;j++)
 {
 if(a[i]<a[j])
 {
-k=a[i];
-a[i]=a[j];
-a[j]=k;
+k=b[i];
+b[i]=b[j];
+b[j]=k;
 }}}
 printf("the maximum no is");
 }
