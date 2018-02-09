2e3a9ffc5661914f54ce508c72a3d7a1da7356b8
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-09 16:06:01 +0530

    Create oddorevenprod.c

---
+++ oddorevenprod.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values");
+scanf("%d %d",&a,&b);
+c=a*b;
+if((c%2)==0)
+printf("even number");
+else
+printf("odd number");
+}
