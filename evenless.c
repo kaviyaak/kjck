526aa8517346f67777266e6a539c35208590543d
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-09 16:33:38 +0530

    Create evenless.c

---
+++ evenless.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the no");
+scanf("%d",&a);
+if((a%2)==0)
+printf("even number is %d",a);
+else
+printf("less than even number is %d",a-1);
+}
