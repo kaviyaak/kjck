882ffb827cd3dabde8c77912b4a00c2b9cc034a8
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-12 16:38:27 +0530

    Create addoddeven.c

---
+++ addoddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,a;
+printf("enter the 1st no");
+scanf("%d",&n);
+printf("enter the 2nd no");
+scanf("%d",&k);
+a=n+k;
+if(a%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+}
