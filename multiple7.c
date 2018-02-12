daabec24125f2f9955a42236176ca750e3659969
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-12 15:58:31 +0530

    Create multiple7.c

---
+++ multiple7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("yes it is multiples of 7");
+}
+else
+{
+printf("no ");
+}
+}
