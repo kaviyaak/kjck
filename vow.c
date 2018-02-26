9f78a01fd9f53b3b83865546509c0a2d633bd350
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-26 16:50:52 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,count=0;
+printf("enter the character");
+scanf("%c",&c);
+for(i=0;i<10;i++)
+{
+if(c=='a'||c=='e'||c=='i'||c=='e'||c=='o'||c=='u')
+{
+count++;
+}}
+if(count>0)
+{
+printf("yes it is vowels");
+}
+else
+{
+printf("no");
+}}
