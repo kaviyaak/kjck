51851578cbade15738f9e7fe5d2b846608b9cb11
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-26 16:28:16 +0530

    Create perfectsq.c

---
+++ perfectsq.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,k,m,i
+printf("enter the first no"):
+scanf("%d",&a);
+printf("enter the second no");
+scanf("%d",&k);
+m=a*k;
+for(i=0;i<=m;i++)
+{
+if(m=i*i)
+printf("yes");
+}
+else
+printf("no");
+}
