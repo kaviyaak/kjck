b1d024a3888bf0537f334a3e08ec8b87a8315de5
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-02-28 16:10:27 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,count=0,i,n;
+  printf("enter the number");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++)
+  {
+    if(n%i==0)
+    
+{
+  count++;
+}
+  }
+  if(count==2)
+  {
+    printf("no");
+  }
+  else
+  {
+    printf("yes");
+  }
+
