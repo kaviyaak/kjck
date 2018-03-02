4095939ed1859e3f4921e964fe714ad0e1603eee
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-03-02 16:10:51 +0530

    Create alphanumeric.c

---
+++ alphanumeric.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[20];
+int i;
+printf("enter the character");
+scanf("%c",&a[i];
+for(i=0;a[i]!=NULL;i++)
+{
+if(a[i]<='0'&& a[i]>='9')
+{
+printf("%c",&a[i]);
+}
+}}
