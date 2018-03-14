484873cdb9d1516d9c847f18877950ddde38fb1a
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-03-14 16:31:47 +0530

    Create kk.c

---
+++ kk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+ 
+ int kabali,opppnent,i,t;
+ for(i=0;i<5;i++)
+ {
+ scanf("%d %d",&kabali,&opppnent);
+  printf("%d ",opppnent-kabali);
+}
+ getch();
+}
