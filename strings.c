1e4e06d4946cc255a00cfed98f9586bbea4ca6fc
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-03-14 16:03:43 +0530

    Create strings.c

---
+++ strings.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	char k[60]="XCODE";
+	int i;
+	for(i=0;i<=6;i++)
+	{
+	if(i%2==0)
+	printf("%c",k[i]);
+	}
+	printf(" ");
+	for(i=1;i<=6;i++)
+	{
+		if(i%2!=0)
+		{
+		printf("%c",a[i]);
+		}
+	}}
