82912f74722500c6b829221cdcc7f15a99889478
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-03-19 16:38:18 +0530

    Create nat.c

---
+++ nat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+sum=sum+i;
+}
+printf("the sum of value is %d",sum);
+}
