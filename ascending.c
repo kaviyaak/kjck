80036d07d8d5c634b4c04030f460a251b4ecc82e
kaviyaak <35135598+kaviyaak@users.noreply.github.com>
2018-03-19 15:59:08 +0530

    Create ascending.c

---
+++ ascending.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,a[10],j;
+	scanf("%d",&n);
+	for(j=1;j<=n;j++)
+	{
+		scanf("%d ",&a[j]);
+		if(a[j]!=i)
+		{
+			printf("%d",j);
+			break;
+		}
+	}}
